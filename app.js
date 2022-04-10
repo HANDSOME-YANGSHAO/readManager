6cc0fb7de79f353532d9b5b96616180747a087be
cuteSheep <2451228780@qq.com>
2022-04-11 01:38:40 +0800

    feat: 登陆界面

--- app.js
+++ app.js
@@ -1,17 +1,12 @@
 // app.js
 App({
   onLaunch() {
-    // 展示本地存储能力
-    const logs = wx.getStorageSync('logs') || []
-    logs.unshift(Date.now())
-    wx.setStorageSync('logs', logs)
 
-    // 登录
-    wx.login({
-      success: res => {
-        // 发送 res.code 到后台换取 openId, sessionKey, unionId
-      }
-    })
+    // 检验是否有本地缓存数据
+    if (wx.getStorageSync('userInfo')) {
+      this.globalData.userInfo = wx.getStorageSync('userInfo')
+    }
+    console.log(this.globalData.userInfo)
   },
   globalData: {
     userInfo: null,
---
+++ pages/login/login.js
@@ -0,0 +1,74 @@
+//获取应用实例
+const app = getApp()
+const FN = require('../../publicFn/public');
+
+Page({
+  data: {
+    clientHeight: '',
+    account: '',
+    password: '',
+    radio: '0', // 0：学生 1：老师
+  },
+  onLoad() {
+    var that = this
+    wx.getSystemInfo({
+      success: function(res) {
+        console.log(res.windowHeight)
+        that.setData({
+          clientHeight: res.windowHeight
+        });
+      }
+    })
+  },
+  // 身份切换
+  onChange(event) {
+    this.setData({
+      radio: event.detail,
+    });
+  },
+  //登录事件
+  onLogin() {
+    let { account, password } = this.data
+    console.log(account, password)
+    if (account === '') {
+      FN.Toast("账号不能为空")
+    } else if (password === '') {
+      FN.Toast("密码不能为空")
+    } else {
+      console.log("发送登陆请求！")
+      const res = {
+        userId: "1",
+        role: "0",
+        account: "2018211822",
+        userName: "李昊洋",
+        classNum: "04031802",
+        sex: "男",
+      }
+      if (res) {
+        wx.setStorageSync('userInfo', res)
+        app.globalData = res
+        FN.Toast("登陆成功!", 1)
+        wx.switchTab({
+          url: '/pages/bookshelf/bookshelf'
+        })
+      }
+    }
+  },
+  // 返回事件
+  onReturn() {
+    wx.navigateBack({
+      delta: 1
+    })
+  },
+  // 双向绑定
+  onAccountInput(e) {
+    this.setData({
+      account: e.detail.value
+    })
+  },
+  onPasswordInput(e) {
+    this.setData({
+      password: e.detail.value
+    })
+  },
+})
